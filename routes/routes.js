90729831472d4852c0d4625f5c3dc2df28ebf20d
GuiLeoIPS <guilherme.pereira.esct@gmail.com>
2023-01-05 15:18:09 +0000

    UPDATE
    - inseri seguintes menus:
        -livro
        - user
               -inseriruserform
               - listar user
    -passagem de dados do inseriruserform pelo servidor a funcionar

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,27 @@ router.get('/ola', function (req, res) {
 
 });
 
+router.get('/users', function (req, res) {
+    res.render('users');
+  });
+
+  router.get('/inseriruserform', function (req, res) {
+    res.render('inseriruserform');
+  });
+
+  router.post('/inseriruser', function (req, res) {
+    var response = {
+        id:req.body.id,
+        user:req.body.user, 
+        pass:req.body.pass
+    };
+    console.log(response);
+    res.redirect('http://localhost:8081');
+
+ });
+
+
+
 
 router.get('*', function(req, res){
     res.redirect('http://localhost:8081/erro.html');
