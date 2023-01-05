3f3484ee1ffddb1dc10c30c44307b0a0abb9dd53
GuiLeoIPS <guilherme.pereira.esct@gmail.com>
2023-01-05 15:57:03 +0000

    adicionar ao array de um json
    inserir dados num array de um json

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,21 @@
 var express = require('express');
-const data = require('..');
+var mustacheExpress = require('mustache-express');
+var app = express();
 var router = express.Router();
+var users = require('../users.json');
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache'); //Extensão dos ficheiros das views
+app.set('views', __dirname + '/view'); 
 
 
 router.get('/ola', function (req, res) {
     res.send('ola');
+
+});
+
+router.get('/erro', function (req, res) {
+    res.render('erro');
 
 });
 
@@ -17,21 +28,24 @@ router.get('/users', function (req, res) {
   });
 
   router.post('/inseriruser', function (req, res) {
-    var response = {
-        id:req.body.id,
-        user:req.body.user, 
-        pass:req.body.pass
-    };
-    console.log(response);
+    const user = {id: 0, user: "", password: "", admin: false};
+    user.id = parseInt(req.body.id);
+    user.user = req.body.user;
+    user.password = req.body.pass;
+    users.push(user);
+    console.log(users)
     res.redirect('http://localhost:8081');
 
  });
 
 
+ router.get('/login', function (req, res) {
+    res.render('login');
 
+});
 
 router.get('*', function(req, res){
-    res.redirect('http://localhost:8081/erro.html');
+    res.redirect('http://localhost:8081/erro');
 });
 
 
