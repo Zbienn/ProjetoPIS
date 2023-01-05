262664de3f9a568f7dbfebeeef66953b69544f41
Leo <leonardozacarias2002@gmail.com>
2023-01-05 14:36:08 +0000

    login

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ app.set('views', __dirname + '/view'); //definição de uma pasta 'public'
 app.use(express.urlencoded({extended: true}));
 var rotas = require('./routes/routes.js');
 
+var mustacheExpress = require('mustache-express');
+
+var path = require('path');
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache'); //Extensão dos ficheiros das views
+app.set('views', __dirname + '/view'); //Indicação de qual a pasta que irá colocar as views
+
 app.get('/', function(req, res) {
-    res.send('Inicio de aplicação');
+    res.render('login', {
+        title: 'Hey',
+        message: 'Hello there!'
+        })
+    
 });
 
 
